0666167eadf17433b10d942e3401f4857fc859d0
SARTHAK-AIML-0182 <itzsarthakhere@gmail.com>
2026-02-20 10:32:34 +0530

    commit

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-console.log("Hello World!");
-document.write("Hello World!");
+// console.log("Hello World!");
+// document.write("Hello World!");
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+// num = "Hello World!";
+// console.log(num);
+// console.log(typeof(num));
+// num = "hello";
+// console.log(num);
+// console.log(typeof(num));
+// num = undefined;
+// console.log(num);
+// console.log(typeof(num));
+const sym1 = symbol(4);
+console.log(sym1);
+const sym2 = symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false")
+
+}
