e42a87f18b517a64b70a00c29d085592b5b09d45
SARTHAK-AIML-0182 <itzsarthakhere@gmail.com>
2026-02-23 23:51:20 +0530

    scriptday2

--- script_day2.js
+++ script_day2.js
@@ -1,46 +1,32 @@
-var a=10; 
-var b=20;
-var linebreak = "<br/>";
+// var a=10; 
+// var b=20;
+// var linebreak = "<br/>";
 
-document.write("(a==b) =>");
-result = (a==b);
-document.write(result);
-document.write(linebreak);
+// document.write("(a==b) =>");
+// result = (a==b);
+// document.write(result);
+// document.write(linebreak);
 
-document.write("(a<b) =>");
-result = (a<b);
-document.write(result);
-document.write(linebreak);
+// document.write("(a<b) =>");
+// result = (a<b);
+// document.write(result);
+// document.write(linebreak);
 
-document.write("(a>b) =>");
-result = (a>b);
-document.write(result);
-document.write(linebreak);
+// document.write("(a>b) =>");
+// result = (a>b);
+// document.write(result);
+// document.write(linebreak);
 
+var count;
 
+document.write("Starting Loop" + "<br />");
 
+for(count = 0; count < 10; count++) {
 
+}
 
+document.write("Current Count : " + count);
 
+document.write("<br />");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const person={
-//     fname: "siya",
-//     fage:21
-// };
-// for(let x in person){
-//     console.log("person details:" + x + ":"+person[x])
-// }
-
+document.write("Loop stopped!");
