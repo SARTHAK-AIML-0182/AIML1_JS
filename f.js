0186e1d8abb7a180851834bed738f06dfefeae71
SARTHAK-AIML-0182 <itzsarthakhere@gmail.com>
2026-02-27 10:30:47 +0530

    object

---
+++ f.js
@@ -0,0 +1,22 @@
+// var res =mul(12,30);
+// function mul(x,y)
+// {
+//     return x*y;
+// }
+// document.write(res);
+
+
+// normal function
+
+function add(a,b){
+    console.log(a+b);
+}
+add(10,20);
+//arrow function
+var add2 =(a,b) => {
+   console.log(a+b);
+};
+add2(102,20);
+//arrow function for single parameters
+var add3 =(a,b) => console.log(a+b);
+ add3(30,20);
---
+++ object.js
@@ -0,0 +1,14 @@
+var person = { 
+    name : John Mac,
+    age:30,
+    isMarried: true,
+    address: {
+        street:'vinay nagar',
+        flatNO: 201
+    }
+}
+// console.log(person)
+console.log(person.name)
+console.log(person.age)
+console.log(person.address)
+console.log(person.street)
