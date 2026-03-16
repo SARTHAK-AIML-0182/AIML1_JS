1de785fbaf1d964d93e660148c589399bfce037d
SARTHAK-AIML-0182 <itzsarthakhere@gmail.com>
2026-03-16 15:50:30 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,11 @@
+// const heading =document.createElement
+// const bodytag =document.getElementsByTagName("body");
+// body[0].aqppend(heading);
+
+// function practice(){
+//     document.open();
+//     document.write("<h1>practice</h1>");
+//     document.close();
+// }
+
+function onMouseover()
